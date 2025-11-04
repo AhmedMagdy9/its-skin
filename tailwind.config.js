d7f51b342ab4d5a7755e2acfe69d43b52e3ae55c
AhmedMagdy9 <megoo9786@gmail.com>
2025-11-04 23:56:20 +0200

    edit tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // 🟢 مهم جدًا علشان Tailwind يقرأ كل ملفاتك
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
